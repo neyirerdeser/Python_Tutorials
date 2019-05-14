c14cfee5ab34d13d70cf1ae7bb3da1f09b93a103
neyirerdeser <neyirerdeser@gmail.com>
2019-05-14 16:48:16 +0300

    cleanup

--- Exercises/11_PrimalityCheck.py
+++ Exercises/11_PrimalityCheck.py
@@ -1,2 +1,11 @@
-input('check if prime: ')
+num = int(input('check if prime: '))
+counter = 0
+for x in range(1, num):
+    # print(num/x, num%x)
+    if num % x == 0:
+        counter += 1
 
+if counter <= 2:
+    print('prime')
+else:
+    print('has ', counter, ' divisors')
